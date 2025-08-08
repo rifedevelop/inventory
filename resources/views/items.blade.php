9b5532d34e26895e3e0564901c90d3875fc1af5c
rifedevelop <rife.develop@gmail.com>
2025-08-08 11:00:47 +0700

    update

--- resources/views/items.blade.php
+++ resources/views/items.blade.php
@@ -81,11 +81,6 @@
                             </div>
                         </form>
                     </div>
-
-                    <!-- <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                        <button type="button" command="close" commandfor="dialog" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto">Deactivate</button>
-                        <button type="button" command="close" commandfor="dialog" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
-                    </div> -->
                 </el-dialog-panel>
             </div>
         </dialog>
@@ -117,9 +112,7 @@
                             <input type="hidden" name="id" id="id-delete">
 
                             <button id="delete-btn" type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto">Delete</button>
-
                         </form>
-
 
                         <button type="button" command="close" commandfor="dialog-delete" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
                     </div>
